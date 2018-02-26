efe30605fe59c65274c0fad63c793b3ff5dd8c06
Vitaliy Filipov <altras@gmail.com>
2018-02-26 21:56:01 +0200

    Update webpack.config.js and move client dna to core/dna/client

--- webpack/dna/client/webpack.config.js
+++ webpack/dna/client/webpack.config.js
@@ -1,47 +1,77 @@
 var webpack = require('webpack')
+var ProgressBarPlugin = require('progress-bar-webpack-plugin')
+var HappyPack = require('happypack')
+
+var happyPackPlugin = function (name, loaders) {
+  return new HappyPack({
+    id: name,
+    verbose: false,
+    threads: require('os').cpus().length,
+    loaders: loaders,
+  })
+}
 
 module.exports = {
-  'resolve': {
-    'extensions': ['', '.webpack.js', '.web.js', '.tag', '.js'],
-    'modulesDirectories': ['web_modules', 'node_modules', 'client/common']
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.tag', '.js'],
+    modules: [
+      'client/common',
+      'node_modules',
+    ],
   },
-  'plugins': [
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new ProgressBarPlugin(),
     new webpack.ProvidePlugin({
-      'oval': 'organic-oval',
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    })
-  ],
-  'module': {
-    'preLoaders': [
+      oval: 'organic-oval',
+      fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch',
+    }),
+    happyPackPlugin('pre-oval', [
+      'organic-oval/webpack/oval-loader',
+      'organic-oval/webpack/oval-control-statements-loader'
+    ]),
+    happyPackPlugin('oval', [
       {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', {modules: false}],
+          ],
+        },
+      },
+    ]),
+    happyPackPlugin('js', [
+      {
+        loader: 'babel-loader',
+        options: {
+          plugins: [
+            ['transform-react-jsx', { pragma: 'createElement' }],
+          ],
+          presets: [
+            ['es2015', {modules: false}],
+          ],
+        },
+      },
+    ]),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
         test: /\.tag$/,
         exclude: /node_modules/,
-        loaders: [
-          'organic-oval/webpack/oval-loader',
-          'organic-oval/webpack/oval-control-statements-loader'
-        ]
-      }
-    ],
-    'loaders': [
+        use: 'happypack/loader?id=pre-oval',
+      },
       {
         test: /\.js|.tag$/,
         include: /node_modules\/organic-oval/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
+        use: 'happypack/loader?id=oval',
       },
       {
         test: /\.js$|\.tag$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          plugins: [
-            ['transform-react-jsx', { pragma: 'createElement' }]
-          ],
-          presets: ['es2015']
-        }
+        use: 'happypack/loader?id=js',
       }
     ]
-  }
+  },
 }
